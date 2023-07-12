a96914afc74ec048cc7619999e302897299fbecd
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-12 10:41:17 -0300

    Add files via upload

---
+++ ex003/Program.cs
@@ -0,0 +1,31 @@
+﻿//3.	Repita o exercício anterior sabendo que os números são diferentes, qual é o maior e o menor dos números.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor inteiro: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite outro valor inteiro: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 != num2)
+        {
+            Console.WriteLine("Os números digitados são diferentes.");
+            if (num1 > num2)
+            {
+                Console.WriteLine($"O maior número é {num1}. \nO menor número é {num2}.");
+            }
+            else
+            {
+                Console.WriteLine($"O maior número é {num2}. \nO menor número é {num1}.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Os números digitados são iguais.");
+        }
+    }
+}
---
+++ ex004/Program.cs
@@ -0,0 +1,21 @@
+﻿//4.	Leia um número e, se ele for positivo, imprima seu inverso; caso contrário imprima seu quadrado Inverso. Inverso: 1/número.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro positivo ou negativo: ");
+        float num = float.Parse(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine(1 / num);
+        }
+        else
+        {
+            Console.WriteLine((1 / num) * (1 / num));
+        }
+    }
+}
