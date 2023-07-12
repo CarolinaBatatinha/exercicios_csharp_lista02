f897682254980d201e34774375c9d0a4e18ab2aa
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-12 10:39:46 -0300

    Add files via upload

---
+++ ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1.	Faça um algoritmo que leia os valores A, B, C e diga se a soma de A + B é menor que C.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor para A: ");
+        int valorA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite um valor para B: ");
+        int valorB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite um valor para C: ");
+        int valorC = Convert.ToInt32(Console.ReadLine());
+
+        if (valorA + valorB < valorC)
+        {
+            Console.WriteLine("O valor da soma de A e B é menor que o valor de C.");
+        }
+    }
+}
---
+++ ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿//2.	Faça um algoritmo para ler dois números é imprimir se eles são iguais, caso contrário, imprimir a mensagem: “os números são diferentes”.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um valor inteiro: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite outro valor inteiro: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 == num2)
+        {
+            Console.WriteLine("Os números digitados são iguais.");
+        }
+        else
+        {
+            Console.WriteLine("Os números digitados são diferentes.");
+        }
+    }
+}
