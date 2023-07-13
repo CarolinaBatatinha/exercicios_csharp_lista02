0b86de0ab2a31d290d6e79f47dc67d1d7cf2810d
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-13 11:08:44 -0300

    Add files via upload

---
+++ ex009/Program.cs
@@ -0,0 +1,24 @@
+﻿// 9.	Faça um algoritmo para verificar se o ano é bissexto.
+
+// ATENÇÃO!
+// Para calcularmos se um ano é bissexto ou não, temos necessariamente que considerar os seguintes fatores: o ano deve ser divisível por quatro, o resto da divisão do ano por 100 tem que ser maior que zero ou a divisão do ano por 400 precisa ser igual a zero.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o ano a ser pesquisado: ");
+        int ano = Convert.ToInt32(Console.ReadLine());
+
+        if ((ano % 4) == 0 || (ano % 100) > 0 || ((ano % 400) == 0))
+        {
+            Console.WriteLine($"{ano} é bissexto.");
+        }
+        else
+        {
+            Console.WriteLine($"{ano} não é bissexto.");
+        }
+    }
+}
