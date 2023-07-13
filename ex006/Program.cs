36d64694a08dbce65b33b119f5b5a668f48e726a
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-13 10:59:47 -0300

    Add files via upload

---
+++ ex006/Program.cs
@@ -0,0 +1,21 @@
+﻿// 6.	Leia um número e imprima se ele é par ou impar.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"O número {num} é par.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} é ímpar.");
+        }
+    }
+}
