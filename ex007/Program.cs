9d50ae1f95ebdb837b0f22e47ef613a5c3bfc168
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-13 11:01:32 -0300

    Add files via upload

---
+++ ex007/Program.cs
@@ -0,0 +1,21 @@
+﻿// 7.	Leia um número para verificar se ele é maior do que 20. Caso afirmativo imprima a metade desse número. Caso contrário imprima o seu quadrado.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        if (num > 20)
+        {
+            Console.WriteLine(num / 2);
+        }
+        else
+        {
+            Console.WriteLine(Math.Pow(num, 2));
+        }
+    }
+}
