117bf4b944c0d895c99ce1daa7565bb78e04839b
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-13 11:04:38 -0300

    Add files via upload

---
+++ ex008/Program.cs
@@ -0,0 +1,25 @@
+﻿// 8.	Leia um número e imprima se ele é positivo, negativo ou nulo.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número inteiro: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine($"O número {num} é positivo.");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine($"O número {num} é negativo.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {num} é nulo.");
+        }
+    }
+}
