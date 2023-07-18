0d501a5faa8fda7534d52e560571ec869b0f1933
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-18 15:15:36 -0300

    Add files via upload

---
+++ ex029/Program.cs
@@ -0,0 +1,61 @@
+﻿// 29.	Elaborar um programa que calcule a média ponderada de um aluno da disciplina de Algoritmo. Esta média tem pesos: 4 para a primeira prova e 3 para a segunda prova. Após calculada a média, uma mensagem deve ser apresentada informando a situação do aluno: APROVADO COM MÉDIA ou NECESSITA FAZER SUBSTITUTIVA. Caso o aluno necessite fazer prova substitutiva, o programa deve pedir esta nota e calcular a nova média do aluno. Uma nova mensagem da situação deve informar ALUNO COM MÉDIA ou ALUNO REPROVADO. Obs: A prova substitutiva pode substituir a primeira prova ou a segunda prova, portanto o programa deve verificar quando o aluno fica com maior média, isto é, quando a primeira prova é substituída pela prova substitutiva ou quando a segunda prova é substituída pela prova substitutiva.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a nota da primeira prova: ");
+        double notaProva1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe a nota da segunda prova: ");
+        double notaProva2 = Convert.ToDouble(Console.ReadLine());
+
+        double media = ((4 * notaProva1) + (3 * notaProva2)) / 7;
+
+        Console.WriteLine($"Média: {media:F1}");
+
+        if (media >= 6)
+        {
+            Console.WriteLine("APROVADO COM MÉDIA");
+        }
+        else
+        {
+            Console.WriteLine("NECESSITA FAZER SUBSTITUTIVA");
+
+            Console.WriteLine("Informe a nota da prova substitutiva:");
+            double notaSubstitutiva = Convert.ToDouble(Console.ReadLine());
+
+            double novaMedia1 = ((4 * notaSubstitutiva) + (3 * notaProva2)) / 7;
+            double novaMedia2 = ((4 * notaProva1) + (3 * notaSubstitutiva)) / 7;
+
+            if (novaMedia1 > novaMedia2)
+            {
+                Console.WriteLine($"Média do aluno após prova substitutiva: {novaMedia1:F1}");
+
+                if (novaMedia1 >= 6)
+                {
+                    Console.WriteLine("ALUNO APROVADO");
+                }
+                else
+                {
+                    Console.WriteLine("ALUNO REPROVADO");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Média do aluno após prova substitutiva: {novaMedia2:F1}");
+
+                if (novaMedia2 >= 6)
+                {
+                    Console.WriteLine("ALUNO APROVADO");
+                }
+                else
+                {
+                    Console.WriteLine("ALUNO REPROVADO");
+                }
+            }
+        }
+    }
+}
---
+++ ex030/Program.cs
@@ -0,0 +1,49 @@
+﻿// 30.	 O Palmeiras deseja aumentar o salário de seus jogadores e comissão técnica para motivá-los na tentiva de subir para a primeira divisão. O ajuste salarial deve obedecer à seguinte tabela:
+
+// Categoria	Salário Atual	Ação
+// Equipe técnica	-	Aumento de 15%
+// Jogadores	0 a R$ 9.000,00	Aumento de 20%
+// 	9.001,00 a 13.000	Aumento de 10%
+// 	13.001 a 18.000	Aumento de 5%
+// 	acima de 18.000	Sem aumento
+
+// Preparar um algoritmo para ler o nome e o salário atual de cada jogador ou técnico e imprimir seu nome, salário atual e salário reajustado.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("Informe seu cargo (1 - Jogador/  2 - Equipe Técnica): ");
+        int cargo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe seu salário atual em reais (R$): ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+
+        double novoSalario = 0;
+
+        if (cargo == 1 && salario <= 9000)
+        {
+            novoSalario = salario * 1.2;
+        }
+        else if (cargo == 1 && salario > 9001 && salario <= 13000)
+        {
+            novoSalario = salario * 1.1;
+        }
+        else if (cargo == 1 && salario > 13001 && salario <= 18000)
+        {
+            novoSalario = salario * 1.05;
+        }
+        else if (cargo == 1 && salario > 18000)
+        {
+            novoSalario = salario;
+        }
+        else if (cargo == 2)
+        {
+            novoSalario = salario * 1.15;
+        }
+
+        Console.WriteLine($"{nome}, seu novo salário valerá R$ {novoSalario:F2}.");
+    }
+}
